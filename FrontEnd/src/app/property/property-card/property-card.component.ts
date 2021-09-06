a1d6dcd3cff91dc93ef50333974d073d5c3828e4
hocine heriche <her.hocine@gmail.com>
2021-09-06 11:25:41 -0500

    initial commit

---
+++ FrontEnd/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: 'app-property-card',
+  templateUrl: './property-card.component.html',
+  styleUrls: ['./property-card.component.css']
+  // template: ``,
+  // styles: [' ']
+})
+
+export class PropertyCardComponent extends Component {
+    Property: any = {
+      "Id": 1,
+      "Type": "House",
+      "Name": "hocine house",
+      "Price": 12000
+    }
+}
